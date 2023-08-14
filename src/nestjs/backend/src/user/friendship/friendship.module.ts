2f99f3e3c1fe774840458070f505e3c94a977de2
Theo Guerin <tguerin@student.42angouleme.fr>
2023-08-14 16:11:16 +0200

    Move friendship to its own controller

--- src/nestjs/backend/src/user/friendship/friendship.module.ts
+++ src/nestjs/backend/src/user/friendship/friendship.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FriendshipController } from './friendship.controller';
 import { Friendship } from './friendship.entity';
 import { FriendshipService } from './friendship.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Friendship])],
-  controllers: [],
+  controllers: [FriendshipController],
   providers: [FriendshipService],
   exports: [FriendshipService],
 })
