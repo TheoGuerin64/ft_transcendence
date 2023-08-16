60c73676138924bc603cd343ff91f2880e7b6ce5
Theo Guerin <tguerin@student.42angouleme.fr>
2023-08-16 02:48:16 +0200

    Add logging to AppGateway and disconnect client if token is invalid

--- src/nestjs/backend/src/app.gateway.ts
+++ src/nestjs/backend/src/app.gateway.ts
@@ -1,15 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 import { AuthService } from './auth/auth.service';
 import { UserStatus } from './user/user.entity';
 import { UserService } from './user/user.service';
-import {
-  WebSocketGateway,
-  WebSocketServer,
-  WsException,
-} from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 
 @WebSocketGateway({ cors: ['http://localhost:8080', 'http://127.0.0.1:8080'] })
 export class AppGateway {
+  private readonly logger = new Logger(AppGateway.name);
+
   constructor(
     private readonly authService: AuthService,
     private readonly userService: UserService,
@@ -19,13 +18,21 @@ export class AppGateway {
   server: Server;
 
   async handleConnection(client: Socket) {
+    if (!client.handshake.auth.token) {
+      this.logger.error('No token provided');
+      client.disconnect(true);
+      return;
+    }
+
     const user = await this.authService.validateJwt(
       client.handshake.auth.token,
     );
     if (user) {
       await this.userService.update(user, { status: UserStatus.ONLINE });
     } else {
-      throw new WsException('Invalid token');
+      this.logger.error("Token doesn't match any user");
+      client.disconnect(true);
+      return;
     }
   }
 
@@ -33,10 +40,9 @@ export class AppGateway {
     const user = await this.authService.validateJwt(
       client.handshake.auth.token,
     );
+
     if (user) {
       await this.userService.update(user, { status: UserStatus.OFFLINE });
-    } else {
-      throw new WsException('Invalid token');
     }
   }
 }
