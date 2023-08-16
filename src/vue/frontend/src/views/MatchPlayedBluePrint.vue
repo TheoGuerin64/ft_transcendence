45265d2a90e6acfb3311349c7c993fbad4cc3fca
averdon <anthonyverdon10@gmail.com>
2023-08-16 14:36:57 +0200

    keep going on css

--- src/vue/frontend/src/views/MatchPlayedBluePrint.vue
+++ src/vue/frontend/src/views/MatchPlayedBluePrint.vue
@@ -7,7 +7,9 @@ import UserAvatar from '@/components/UserAvatar.vue'
 export default {
   data() {
     return {
-      store
+      store,
+      boxClass:
+        'box column is-flex is-three-fifths is-offset-one-fifth is-justify-content-space-between has-background-danger"'
     }
   },
   props: {
@@ -18,18 +20,44 @@ export default {
     playerTwoScore: Number,
     playerTwoAvatar: String,
     win: Boolean
+  },
+  mounted() {
+    if (this.userWon() === true) {
+      this.boxClass += ' has-background-success'
+    } else {
+      this.boxClass += ' has-background-danger'
+    }
+  },
+  methods: {
+    userWon(): boolean {
+      if (
+        (this.store.user?.name === this.playerOneLogin && this.playerOneScore === 5) ||
+        (this.store.user?.name === this.playerTwoLogin && this.playerTwoScore === 5)
+      ) {
+        return true
+      } else {
+        return false
+      }
+    }
   }
 }
 </script>
 <template>
-  <div class="box is-flex is-justify-content-space-between">
+  <div :class="boxClass">
     <div>
-      <UserAvatar :size="256" :image="playerOneAvatar" />
-      {{ playerOneLogin }} {{ playerOneScore }}
+      <UserAvatar :size="128" :image="playerOneAvatar" />
+      <div class="is-size-4 has-text-centered">
+        {{ playerOneLogin }}
+      </div>
+    </div>
+    <div class="is-size-2 pt-5 mt-5">
+      <p>{{ playerOneScore }} - {{ playerTwoScore }}</p>
     </div>
     <div>
-      <UserAvatar :size="256" :image="playerTwoAvatar" />
-      {{ playerTwoLogin }} {{ playerTwoScore }}
+      <UserAvatar :size="128" :image="playerTwoAvatar" />
+      <div class="is-size-4 has-text-centered">
+        {{ playerTwoLogin }}
+      </div>
     </div>
   </div>
 </template>
